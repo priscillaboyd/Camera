82d16fdb68d13c8dfac123e9591729d47bcc09a2
blah <crcooper@outlook.com>
2017-07-11 15:15:28 +0100

    Mho: shuttcount test added

--- src/test/java/com/develogical/camera/CameraTest.java
+++ src/test/java/com/develogical/camera/CameraTest.java
@@ -16,4 +16,22 @@ public class CameraTest {
         verify(mockedSensor).powerUp();
     }
 
+    @Test
+    public void switchingTheCameraOfPowersDownTheSensor() {
+        Sensor mockedSensor = mock(Sensor.class);
+        Camera camera = new Camera(mockedSensor);
+        camera.powerOn();
+        camera.powerOff();
+        verify(mockedSensor).powerDown();
+    }
+
+    @Test
+    public void pressingTheShutterWhenThePowerIsOffDoesNothing() {
+        Sensor mockedSensor = mock(Sensor.class);
+        Camera camera = new Camera(mockedSensor);
+        camera.powerOff();
+        camera.pressShutter();
+        assertThat(camera.shutterCount, is(0));
+    }
+
 }
