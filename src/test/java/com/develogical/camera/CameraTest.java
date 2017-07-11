50e8ceb134987bb285d3636714801ecc84095023
blah <crcooper@outlook.com>
2017-07-11 14:44:41 +0100

    Mho : initial version of switching camera on routine

--- src/test/java/com/develogical/camera/CameraTest.java
+++ src/test/java/com/develogical/camera/CameraTest.java
@@ -3,11 +3,14 @@ package com.develogical.camera;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 import static org.mockito.Mockito.*;
 
 public class CameraTest {
     @Test
     public void switchingTheCameraOnPowersUpTheSensor() {
-        // write your test here
+        Camera camera = new Camera();
+        assertThat(camera.on(), is(true));
     }
 }
